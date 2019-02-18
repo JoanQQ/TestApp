04dc20c4e6d10702834580536c74ebb2664b6a55
liq.zhao <liq.zhao@hnair.com>
2019-02-18 14:51:49 +0800

    Start MinePage test

--- appium/java/AppTesting/src/test/java/MineTest.java
+++ appium/java/AppTesting/src/test/java/MineTest.java
@@ -1,25 +1,49 @@
 import junit.framework.TestCase;
-import org.junit.Test;
-import pages.MainPage;
-import pages.MinePage;
+import org.junit.*;
+import pages.*;
 import utils.SingleDriver;
-
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author Phyllis
- * @Title ${file_name}
- * @Package ${package_name}
- * @Description ${todo}
  * @date 2019-02-14 14:03
  */
 public class MineTest extends TestCase {
-    @Test
-    public void testMine() {
+    @BeforeClass
+    public static void beforeClass() {
         SingleDriver.getInstance();
-        MainPage mainPage = new MainPage();
-        MinePage minePage = mainPage.gotoMinePage();
-        SingleDriver.getInstance().manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
-        minePage.gotoAddressPage();
+        GdprPage gdprPage = new GdprPage ( );
+        AdsPage adsPage = gdprPage.gotoAdsPage ( );
+        MainPage mainPage = adsPage.gotoMainPage ( );
+        final MinePage minePage = mainPage.gotoMinePage ( );
+    }
+
+    @Test
+    public void testLogin() {
+        MinePage minePage = new MinePage();
+        LoginPage loginPage = minePage.gotoLoginPage();
+        MinePage minePage2 = loginPage.gotoMinePage("7050812098","121321");
+        SingleDriver.getInstance().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        assertEquals("LIQIONGTEST ZHAOTEST", minePage2.getUsername());
+        System.out.println("Username is correct");
+        assertEquals("金卡 7050812098", minePage2.getFfpid());
+        System.out.println("FFP card level and ID are correct");
+        assertEquals("可用积分 12881.1", minePage2.getScore());
+        System.out.println("Available point is correct");
+        assertEquals ("已实名认证", minePage2.getCertificationStatus ());
+        System.out.println("Certification is correct");
+    }
+
+    @Test
+    public void testCenterMenu() {
+        MinePage minePage = new MinePage ();
+        minePage.backFromOrder();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        SingleDriver.quit ();
+        System.out.println("@AfterClass");
     }
 }
