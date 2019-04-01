b4528fba1a8ca11318b9d522200f78908fe9ccf4
liq.zhao <liq.zhao@hnair.com>
2019-04-01 17:32:52 +0800

    App

---
+++ java/AppInterfaceTesting/src/main/java/Configuaration/CustomVariable.java
@@ -0,0 +1,16 @@
+package Configuaration;
+
+/**
+ * @author Phyllis
+ * @Title ${file_name}
+ * @Package ${package_name}
+ * @Description ${todo}
+ * @date 2019-04-01 15:10
+ */
+public class CustomVariable {
+    public static String nightlyServer = "https://hna-app-test.hnair.com/mapp";
+    public static String betaServer = "https://hna-app-test.hnair.com/mapp-prd";
+
+    public static String hnairSign = "5D122C6F1449E8AB69A37E34F34067DDA5C98EE7";
+    public static String desc = "iat%2BQ5z8%2B4dasDscyyGDjMM2vOwzkmj4/HYHyt4USMauWGXi4GaYXc/6BlJ5ZcZb8igVVfhoWn6PVCSEJ5trTCEXZ5TEWmg8fq7qr5Gp2wNiC54o%2BkJH5jGXZ2bSYfOiP8VZR18QAicdVU8RRaljHgylMXRYM2Y0XBPUMR4eJBlrbluIQI%2BafjQ%2BfllElsO0pHQH5KvrzwsbcAPYHylZybLuNEYrX0MD5U6dolODj8U=";
+}
---
+++ java/AppInterfaceTesting/src/test/java/tmp/Message.java
@@ -0,0 +1,20 @@
+package tmp;
+
+/**
+ * @author Phyllis
+ * @Title ${file_name}
+ * @Package ${package_name}
+ * @Description ${todo}
+ * @date 2019-04-01 12:04
+ */
+public class Message {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
